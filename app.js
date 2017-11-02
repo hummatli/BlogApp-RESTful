c5faea756198cc6377ca6325dec06c57da845aec
Sattar Hummatli <settarxan@gmail.com>
2017-11-02 12:42:50 +0000

    Styled index page

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-var bodyParser      = require("body-parser"),
-    methodOverride  = require("method-override"),
-    mongoose        = require("mongoose"),
-    express         = require("express"),
-    app             = express()
+var bodyParser          = require("body-parser"),
+    methodOverride      = require("method-override"),
+    expressSanitizer    = require("express-sanitizer"),
+    mongoose            = require("mongoose"),
+    express             = require("express"),
+    app                 = express()
     
     
 //APP CONFIG
@@ -12,6 +13,7 @@ mongoose.Promise = global.Promise
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(expressSanitizer())
 app.use(methodOverride("_method"))
 
 //MONGOOSE/MODEL CONFIG
@@ -47,6 +49,7 @@ app.get("/blogs/new", function(req, res) {
 //CREATE ROUTE
 app.post("/blogs", function(req, res) {
     //create blog
+    req.body.blog.body = req.sanitizer(req.body.blog.body)
     Blog.create(req.body.blog, function(err, newBlog) {
         if(err) {
             res.render("new")
@@ -83,6 +86,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
+    req.body.blog.body = req.sanitizer(req.body.blog.body)
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
         if(err) {
             res.redirect("/blogs")

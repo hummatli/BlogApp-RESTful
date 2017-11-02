908247f9b1ec04c11b5706d232b01a3d3322b04a
Sattar Hummatli <settarxan@gmail.com>
2017-11-02 08:32:56 +0000

    created new blog form and create route

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.get("/", function(req, res) {
     res.redirect("/blogs")
 })
 
+//INDEX REOUTE
 app.get("/blogs", function(req, res) {
     Blog.find({}, function(err, blogs) {
         if(err) {
@@ -36,6 +37,24 @@ app.get("/blogs", function(req, res) {
     })
 })
 
+//NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+    res.render("new")
+})
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res) {
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err) {
+            res.render("new")
+        } else {
+            //redirect to the index
+            res.redirect("/blogs")
+        }
+    })
+
+})
 
 //LISTEN
 app.listen(process.env.PORT, process.env.IP, function() {

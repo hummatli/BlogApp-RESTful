f648f43e52eeb86204ba5cba2309f3eb6dc9e9f0
Sattar Hummatli <settarxan@gmail.com>
2017-11-02 13:06:02 +0000

    Added screenshot

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.get("/blogs/new", function(req, res) {
 //CREATE ROUTE
 app.post("/blogs", function(req, res) {
     //create blog
-    req.body.blog.body = req.sanitizer(req.body.blog.body)
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.create(req.body.blog, function(err, newBlog) {
         if(err) {
             res.render("new")
@@ -86,7 +86,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res) {
-    req.body.blog.body = req.sanitizer(req.body.blog.body)
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
         if(err) {
             res.redirect("/blogs")

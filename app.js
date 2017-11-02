4a327fe2a46b9f9afe04b4f83b5278dd269e2e30
Sattar Hummatli <settarxan@gmail.com>
2017-11-02 11:35:18 +0000

    Added put route, to support it added method-override modul

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    express     = require("express"),
-    app         = express()
+var bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require("mongoose"),
+    express         = require("express"),
+    app             = express()
     
     
 //APP CONFIG
@@ -11,6 +12,7 @@ mongoose.Promise = global.Promise
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride("_method"))
 
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
@@ -66,6 +68,28 @@ app.get("/blogs/:id", function(req, res) {
         }
     })
     
+})
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if(err) {
+            res.redirect("/blogs")
+        } else {
+            res.render("edit", {blog: foundBlog})   
+        }
+    })
+})
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+        if(err) {
+            res.redirect("/blogs")
+        } else {
+            res.redirect("/blogs/" + req.params.id)
+        }
+    })
 })
 
 //LISTEN
